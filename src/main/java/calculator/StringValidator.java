b216e42ca7e47e9665e022682592a43d3a8288b0
GitDohyun <GitDohyun@gmail.com>
2025-10-20 02:51:15 +0900

    feat: 문자열 검사
    StringValidator에  기능 추가
    - 하나의 양수만 입력한 경우 구현
    - 양수가 아닌 경우 IllegalArgumentException을 발생시킨 후 애플리케이션을 종료

--- src/main/java/calculator/StringValidator.java
+++ src/main/java/calculator/StringValidator.java
@@ -6,10 +6,23 @@ public class StringValidator {
             return "결과 : 0";
         }
 
-        return "결과 : ";
+        long positiveInteger = parsePositiveInteger(input);
+        return "결과 : " + positiveInteger;
     }
 
     private boolean isEmpty(String input) {
         return input == null || input.trim().isEmpty(); //공백도 확인하기 위해 trim 사용
+    }
+
+    private long parsePositiveInteger(String input) {
+        try {
+            long positiveInteger = Long.parseLong(input);
+            if (positiveInteger < 0) {
+                throw new IllegalArgumentException();
+            }
+            return positiveInteger;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
