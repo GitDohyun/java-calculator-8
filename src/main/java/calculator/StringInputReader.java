db03b2a41122240b6e33f6623cd3956a04e81ca6
GitDohyun <GitDohyun@gmail.com>
2025-10-20 02:07:01 +0900

    style: StringInputReader 출력 메시지 오타 수정
    StringInputReader 출력 메시지의 '덧겜할 ~'오타 수정

--- src/main/java/calculator/StringInputReader.java
+++ src/main/java/calculator/StringInputReader.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class StringInputReader {
     public String readStringInput() {
-        System.out.print("덧겜할 문자열을 입력해 주세요.\n");
+        System.out.print("덧셈할 문자열을 입력해 주세요.\n");
         return Console.readLine();
     }
 }
